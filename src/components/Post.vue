2699e12a2eb7e5ee3705d3a491c0bb389cc3c614
Hasin Hayder <countdraculla@gmail.com>
2023-09-11 19:50:49 +0600

    Params

---
+++ src/components/Post.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <h2>This is post page</h2>
+    <h3>Post id is {{ $route.params.id }}</h3>
+</template>    
+<style></style>
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import About from '../components/About.vue'
 import Contact from '../components/Contact.vue'
+import Post from '../components/Post.vue'
 const routes = [
     { path: '/', component: HelloWorld },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
+    { path: '/post/:id', component: Post }
 ]
 
 const router= createRouter({
