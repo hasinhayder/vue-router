2a90381ae75c9db477613030075a2d44f67137ce
Hasin Hayder <countdraculla@gmail.com>
2023-09-11 20:39:34 +0600

    Not Found

--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import About from '../components/About.vue'
 import Contact from '../components/Contact.vue'
+import NotFound from '../components/NotFound.vue'
 const routes = [
     { path: '/', component: HelloWorld },
     { path: '/about', component: About },
     { path: '/contact', component: Contact },
+    { path: '/:pathMatch(.*)*', component: NotFound },
 ]
 
 const router= createRouter({
