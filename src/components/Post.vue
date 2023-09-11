6e97f4df4a4eadd1f4406eed185fbf584eae92f1
Hasin Hayder <countdraculla@gmail.com>
2023-09-11 19:57:19 +0600

    Dynamic Item

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,8 +1,23 @@
 <script setup>
+import {ref, reactive, onBeforeMount} from 'vue'
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const post = reactive({
+    title:''
+})
+
+onBeforeMount(()=>{
+    fetch(`https://dummyjson.com/posts/${route.params.id}`)
+    .then(response => response.json())
+    .then(json => {
+        post.title = json.title
+    })
+})
 </script>
 
 <template>
     <h2>This is post page</h2>
     <h3>Post id is {{ $route.params.id }}</h3>
+    <h3>Post title is {{ post.title }}</h3>
 </template>    
 <style></style>
